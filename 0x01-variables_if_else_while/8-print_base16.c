b625b98191d816ea1af5e6239fc3e3960aa3f6b2
otukam <otukamtrinity@gmail.com>
2022-10-16 06:49:12 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,12 +5,13 @@
  */
 int main(void)
 {
-		int i;
-			char hexvalues[] = "0123456789abcdef";
-			for (i = 0; i < 16; i++)
-			{
-				putchar(hexvalues[i]);
-			}
-			putchar('\n');
-			return (0);
+	int i;
+		char hexvalues[] = "0123456789abcdef";
+
+		for (i = 0; i < 16; i++)
+		{
+			putchar(hexvalues[i]);
+		}
+		putchar('\n');
+		return (0);
 }
