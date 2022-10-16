06b6e3f767791b33e531ec2e99666f2684482303
otukam <otukamtrinity@gmail.com>
2022-10-16 03:40:46 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/*
- *  *Main- entry point for the programm
- *   */
- /*
+/**
+ *  *main- entry point for the programm
+ *   
   * *Return: returns zero if no erros otherwise returns non-zero value.
- *     */
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
